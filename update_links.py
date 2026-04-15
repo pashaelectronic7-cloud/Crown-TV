27189a34cafa6d1fe9042951c7c83b371cf9caa7
pashaelectronic7-cloud <pashaelectronic7@gmail.com>
2026-04-15 19:13:39 +0300

    Update update_links.py

--- update_links.py
+++ update_links.py
@@ -1,9 +1,10 @@
 import requests
 
-# لیستی کەناڵەکان و لینکەکانیان
+# لیستی کەناڵەکان و لینکە ئەسڵییەکانیان
 channels = {
     "AVA_TV": "https://ava3.store/upload/ava.m3u8",
-    "beIN_Sports_1": "https://3syrialive.s3.us-east-2.amazonaws.com/bein1/master.m3u8"
+    "beIN_Sports_1": "https://3syrialive.s3.us-east-2.amazonaws.com/bein1/master.m3u8",
+    "KurdSat": "https://d1x82nydcxndze.cloudfront.net/live/index.m3u8"
 }
 
 try:
@@ -12,6 +13,6 @@ try:
         for name, url in channels.items():
             f.write(f"#EXTINF:-1,{name}\n")
             f.write(f"{url}\n")
-    print("تەواو! لینکەکان نوێ کرانەوە")
+    print("Success: All channels updated.")
 except Exception as e:
     print(f"Error: {e}")
